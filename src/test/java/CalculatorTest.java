eac3ff6974ef28effcfe3e920b2bfe26eb0f37ca
rosewhat <whatcodingrose@gmail.com>
2022-10-06 13:01:58 +0300

    errorTestLengthValues

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -19,5 +19,14 @@ public class CalculatorTest {
         Assert.assertEquals(expected, calculator.sum(firstValue, secondValue), EPS);
     }
 
+    @Test(expected = ArithmeticException.class)
+    public void checkLengthValues() {
+        int firstValue = 4;
+        int secondValue = 3;
+        int expected = 5;
+        Assert.assertEquals(expected, calculator.sum(firstValue, secondValue), EPS);
+    }
+
+
 
 }
