1f9ab510ccc116c47ceba6a5eaad845e25b2b487
rosewhat <whatcodingrose@gmail.com>
2022-10-06 12:45:15 +0300

    Initial first commit exampleModel

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+
+
+    private int sum(int firstValue, int secondValue) {
+        return 0;
+    }
+
+    private double length(int firstValue, int secondValue) {
+        return 0;
+    }
+
+    private int divide(int firstValue, int secondValue) {
+        return 0;
+    }
+
+    private int density(int weight, int volume) {
+        return 0;
+    }
+
+
+}
